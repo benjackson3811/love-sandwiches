a4f7a1cd3791c904b6c751d568fdb5ea03d8df80
benjackson3811 <benjackson3811@googlemail.com>
2023-02-27 13:23:54 +0000

    refactor worksheet functions into one, old funcitons commented out

--- run.py
+++ run.py
@@ -64,6 +64,16 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def update_worksheet(data, worksheet):
+    """
+    Recieves a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
 def update_surplus_worksheet(data):
     """
     Update surplus worksheet, add new row with the list data provided
@@ -98,9 +108,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
     
 
 print('Welcome to love Sandwiches Data Automation')
